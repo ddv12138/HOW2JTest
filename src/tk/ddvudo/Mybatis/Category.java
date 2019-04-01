129fa6fc3a50a0e059473cf3264ad92679ace357
ddvudo <644077730@qq.com>
2019-04-01 11:57:13 +0800

    一对多一直不成功的原因找到了，javaBean必须要有不带参数的构造函数，因为查询结果组装会调用，有参构造覆盖了无参构造，而有参构造的参数顺序与sql结果不符就报错

--- src/tk/ddvudo/Mybatis/Category.java
+++ src/tk/ddvudo/Mybatis/Category.java
@@ -5,29 +5,12 @@ import java.util.List;
 public class Category {
     private String id;
     private String name;
-    private List<Product> products;
 
-    public String getId() {
-        return id;
-    }
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
     public List<Product> getProducts() {
         return products;
     }
 
-    public void setProducts(List<Product> products) {
-        this.products = products;
-    }
-
+    private List<Product> products;
 
     @Override
     public String toString() {
--- src/tk/ddvudo/Mybatis/Product.java
+++ src/tk/ddvudo/Mybatis/Product.java
@@ -1,49 +1,9 @@
 package tk.ddvudo.Mybatis;
 
-import java.util.UUID;
 
 public class Product {
     String id, name, cid;
     Float price;
-
-    public Product(String name, String cid, Float price) {
-        this.id = UUID.randomUUID().toString().replaceAll("-", "");
-        this.name = name;
-        this.cid = cid;
-        this.price = price;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCid() {
-        return cid;
-    }
-
-    public void setCid(String cid) {
-        this.cid = cid;
-    }
-
-    public Float getPrice() {
-        return price;
-    }
-
-    public void setPrice(Float price) {
-        this.price = price;
-    }
 
     @Override
     public String toString() {
