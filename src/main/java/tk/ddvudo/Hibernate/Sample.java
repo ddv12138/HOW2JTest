2f7bf7f3b6a535b231ee3da4322097e2b18220e4
ddvudo <644077730@qq.com>
2019-06-20 15:45:25 +0800

    :ok_hand: Updating code due to code review changes.

--- src/main/java/tk/ddvudo/Hibernate/Sample.java
+++ src/main/java/tk/ddvudo/Hibernate/Sample.java
@@ -3,17 +3,14 @@ package tk.ddvudo.Hibernate;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.query.Query;
 
 public class Sample {
     public static void main(String... args) {
         SessionFactory sf = new Configuration().configure().buildSessionFactory();
         Session session = sf.openSession();
         session.beginTransaction();
-        Query query = session.createQuery("from Category p where p.name like :namelike");
-        query.setParameter("namelike", "20190619%");
-        query.list();
-        System.out.println(query.list());
+        session.createQuery("select p from Category p where p.name like :name")
+                .setParameter("name", "20190619%").list();
         session.getTransaction().commit();
         session.close();
         sf.close();
