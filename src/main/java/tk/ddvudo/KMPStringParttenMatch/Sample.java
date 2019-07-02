3828a9aa1b4ee86311b80d855efdc6807cc8ec21
ddvudo <644077730@qq.com>
2019-07-02 17:16:42 +0800

    kmp算法

--- src/main/java/tk/ddvudo/KMPStringParttenMatch/Sample.java
+++ src/main/java/tk/ddvudo/KMPStringParttenMatch/Sample.java
@@ -3,23 +3,28 @@ package tk.ddvudo.KMPStringParttenMatch;
 public class Sample {
     public static void main(String... args) {
         String p = "ababaaaba";
+        //011234223
         int[] next = get_next(p);
         for (int i : next) {
             System.out.print(i + " , ");
         }
     }
 
-    private static int[] get_next(String p) {
-        int[] next = new int[p.length() + 1];
-        int i = 1, j = 0;
-        next[1] = 1;
-        while (i < p.length()) {
-            if (j == 0 || p.charAt(i) == p.charAt(j)) {
-                i++;
-                j++;
-                next[i] = j;
-            } else {
-                j = next[j];
+    private static int[] get_next(String t) {
+        int[] next = new int[t.length()];
+        next[0] = -1;
+        next[1] = 0;
+        int k;
+        for (int j = 2; j < t.length(); j++) {
+            k = next[j - 1];
+            while (k != -1) {
+                if (t.charAt(j - 1) == t.charAt(k)) {
+                    next[j] = k + 1;
+                    break;
+                } else {
+                    k = next[k];
+                }
+                next[j] = 0;  //当k==-1而跳出循环时，next[j] = 0，否则next[j]会在break之前被赋值
             }
         }
         return next;
