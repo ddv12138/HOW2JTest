9a64c25dd2c6fd57cef5679c21b302a6c6f3eb01
ddvudo <644077730@qq.com>
2019-08-08 18:40:17 +0800

    :ok_hand: Updating code due to code review changes.

--- src/main/java/tk/ddvudo/Redis/Mysql2RedisSample.java
+++ src/main/java/tk/ddvudo/Redis/Mysql2RedisSample.java
@@ -5,14 +5,13 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import redis.clients.jedis.Jedis;
 import tk.ddvudo.Mybatis.JavaBeans.Enterprise;
 import tk.ddvudo.Mybatis.JavaBeans.EnterpriseExample;
 import tk.ddvudo.Mybatis.UseAnnotation.EnterpriseDao;
 
 import java.io.IOException;
+import java.util.List;
 
 public class Mysql2RedisSample {
     private SqlSessionFactory sqlSessionFactory;
@@ -30,8 +29,47 @@ public class Mysql2RedisSample {
     }
 
     public static void main(String... args) {
-        Logger logger = LoggerFactory.getLogger(Mysql2RedisSample.class);
-        logger.info("aaa");
+        singleThread();
+    }
+
+    static void singleThread() {
+        Mysql2RedisSample mysql2RedisSample = new Mysql2RedisSample();
+        try (SqlSession session = mysql2RedisSample.sqlSessionFactory.openSession()) {
+            EnterpriseDao enterMapper = session.getMapper(EnterpriseDao.class);
+            EnterpriseExample enterpriseExample = new EnterpriseExample();
+            enterpriseExample.setOrderByClause("id desc");
+            long count = enterMapper.countByExample(enterpriseExample);
+            int pageSize = 10000;
+            for (int i = 0; i * pageSize < count; i++) {
+                long offset = (i * pageSize);
+                enterpriseExample.setOffset(offset);
+                enterpriseExample.setLimit(pageSize);
+                List<Enterprise> res = enterMapper.selectByExample(enterpriseExample);
+                new Thread(() -> {
+                    for (Enterprise enterprise : res) {
+                        System.out.println("已处理id=" + enterprise.getId() + ",线程");
+                        mysql2RedisSample.jedis.hsetnx("Enterprise", enterprise.getKey(), JSON.toJSONString(enterprise));
+                    }
+                }).run();
+            }
+        }
+    }
+
+    static void singleThreadStream() {
+        Mysql2RedisSample mysql2RedisSample = new Mysql2RedisSample();
+        try (SqlSession session = mysql2RedisSample.sqlSessionFactory.openSession()) {
+            EnterpriseDao enterMapper = session.getMapper(EnterpriseDao.class);
+            EnterpriseExample enterpriseExample = new EnterpriseExample();
+            enterpriseExample.setOrderByClause("id desc");
+            enterMapper.selectByExample_Map_Forward(enterpriseExample, resultContext -> {
+                Enterprise enterprise = resultContext.getResultObject();
+                System.out.println("已处理id=" + enterprise.getId());
+                mysql2RedisSample.jedis.hsetnx("Enterprise", enterprise.getKey(), JSON.toJSONString(enterprise));
+            });
+        }
+    }
+
+    static void mutilThread() {
         Mysql2RedisSample mysql2RedisSample = new Mysql2RedisSample();
         try (SqlSession session = mysql2RedisSample.sqlSessionFactory.openSession()) {
             EnterpriseDao enterMapper = session.getMapper(EnterpriseDao.class);
@@ -40,9 +78,6 @@ public class Mysql2RedisSample {
             int pageSize = 100 * 10000;
             for (int i = 0; i * pageSize < count; i++) {
                 long offset = (i * pageSize);
-                if (offset >= count) {
-                    offset = count;
-                }
                 final long finalOffset = offset;
                 new Thread(() -> {
                     try (SqlSession session_tmp = mysql2RedisSample.sqlSessionFactory.openSession()) {
