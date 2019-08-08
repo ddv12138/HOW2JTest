098419a50670f3f32f6fb2f3f753dc13c872f4da
ddvudo <644077730@qq.com>
2019-08-08 14:18:27 +0800

    :ok_hand: Updating code due to code review changes.

--- src/main/java/tk/ddvudo/Redis/Mysql2RedisSample.java
+++ src/main/java/tk/ddvudo/Redis/Mysql2RedisSample.java
@@ -1,5 +1,39 @@
 package tk.ddvudo.Redis;
 
-public class Mysql2RedisSample {
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import redis.clients.jedis.Jedis;
+import tk.ddvudo.Mybatis.UseAnnotation.EnterpriseDao;
 
+import java.io.IOException;
+import java.util.Map;
+
+public class Mysql2RedisSample {
+    private String resource = "mybatis-config.xml";
+    private SqlSessionFactory sqlSessionFactory;
+    private Jedis jedis;
+
+    public Mysql2RedisSample() {
+        try {
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(Resources.getResourceAsStream(resource));
+            jedis = new Jedis("188.131.157.4", 6379);
+            jedis.auth("liukang951006");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String... args) {
+        Logger logger = LoggerFactory.getLogger(Mysql2RedisSample.class);
+        Mysql2RedisSample mysql2RedisSample = new Mysql2RedisSample();
+        try (SqlSession session = mysql2RedisSample.sqlSessionFactory.openSession()) {
+            EnterpriseDao enterMapper = session.getMapper(EnterpriseDao.class);
+            Map enterprise = enterMapper.selectByPrimaryKey_Map(1);
+            System.out.println(enterprise);
+        }
+    }
 }
