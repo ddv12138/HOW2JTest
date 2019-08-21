4335e37dbe8edc302297c82e2ae96240f0b24755
ddvudo <644077730@qq.com>
2019-08-21 17:32:03 +0800

    :ok_hand: Updating code due to code review changes.

--- src/main/java/tk/ddvudo/Geode/mutliThreadSample.java
+++ src/main/java/tk/ddvudo/Geode/mutliThreadSample.java
@@ -1,6 +1,5 @@
 package tk.ddvudo.Geode;
 
-import com.alibaba.fastjson.JSON;
 import org.apache.geode.cache.Region;
 import org.apache.geode.cache.client.ClientCache;
 import org.apache.geode.cache.client.ClientCacheFactory;
@@ -11,11 +10,9 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.apache.log4j.Logger;
-import redis.clients.jedis.Jedis;
 import tk.ddvudo.Mybatis.JavaBeans.Enterprise;
 import tk.ddvudo.Mybatis.JavaBeans.EnterpriseExample;
 import tk.ddvudo.Mybatis.UseAnnotation.EnterpriseDao;
-import tk.ddvudo.Redis.Mysql2RedisSample;
 
 import java.io.IOException;
 import java.util.List;
@@ -40,7 +37,7 @@ public class mutliThreadSample {
 
     private static void singleThread() throws IOException {
         Logger logger = Logger.getRootLogger();
-        ExecutorService pool = Executors.newCachedThreadPool();
+        ExecutorService pool = Executors.newFixedThreadPool(4);
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(Resources.getResourceAsStream("mybatis-config.xml"));
         ClientCache cache = null;
         try (SqlSession session = sqlSessionFactory.openSession()) {
@@ -48,25 +45,19 @@ public class mutliThreadSample {
             EnterpriseExample enterpriseExample = new EnterpriseExample();
             enterpriseExample.setOrderByClause("id desc");
             long count = enterMapper.countByExample(enterpriseExample);
-            count = 100000;
             int pageSize = 1000;
             cache = new ClientCacheFactory().addPoolLocator("localhost", 10334).set("log-level", "ERROR").create();
             ClientRegionFactory rf = cache.createClientRegionFactory(ClientRegionShortcut.PROXY);
             Region region = rf.create(regionName);
             for (int i = 0; i * pageSize < count; i++) {
                 long offset = (i * pageSize);
+                enterpriseExample.setOffset(offset);
+                enterpriseExample.setLimit(pageSize);
+                List<Enterprise> res = enterMapper.selectByExample(enterpriseExample);
                 pool.submit(() -> {
-                    try (SqlSession session_tmp = sqlSessionFactory.openSession()) {
-                        EnterpriseDao enterMapper_tmp = session_tmp.getMapper(EnterpriseDao.class);
-                        EnterpriseExample enterpriseExample_tmp = new EnterpriseExample();
-                        enterpriseExample_tmp.setOrderByClause("id desc");
-                        enterpriseExample_tmp.setOffset(offset);
-                        enterpriseExample_tmp.setLimit(pageSize);
-                        List<Enterprise> res = enterMapper_tmp.selectByExample(enterpriseExample_tmp);
-                        for (Enterprise enterprise : res) {
-                            logger.info("已处理id=" + enterprise.getId() + ",线程" + enterprise.toString());
-                            region.put(enterprise.getKey(), enterprise);
-                        }
+                    for (Enterprise enterprise : res) {
+                        logger.info("已处理id=" + enterprise.getId() + ",线程" + enterprise.toString());
+                        region.put(enterprise.getKey(), enterprise);
                     }
                 });
             }
