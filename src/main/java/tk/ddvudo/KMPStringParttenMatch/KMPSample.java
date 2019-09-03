73faf4a3c5fa02e5b680acfcd01e1aa35ede952b
ddvudo <644077730@qq.com>
2019-09-03 15:12:48 +0800

    :ok_hand: Updating code due to code review changes.

--- src/main/java/tk/ddvudo/KMPStringParttenMatch/KMPSample.java
+++ src/main/java/tk/ddvudo/KMPStringParttenMatch/KMPSample.java
@@ -1,5 +1,6 @@
 package tk.ddvudo.KMPStringParttenMatch;
 
+import com.alibaba.fastjson.JSON;
 import com.sun.istack.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,24 +9,26 @@ public class KMPSample {
     private static Logger logger = LoggerFactory.getLogger(KMPSample.class.getName());
 
     public static void main(String... args) {
-        String p = "ababbaaa";
-        int[] next = new int[]{0, 0, 1, 2, 0, 0, 0, 0};
-//        logger.info(violentMatch(p, "baaa") + "");
-        logger.info(KMPMatch(p, "baaa", next) + "");
+        String s = "abababaaa";
+        String p = "abaa";
+        int[] next = get_next(s);
+        logger.info("next-->" + JSON.toJSONString(next));
+        logger.info(violentMatch(s, p) + "");
+        logger.info(KMPMatch(s, p, next) + "");
     }
 
     private static int KMPMatch(String p, String t, int[] next) {
-        int i = 0, j = 0;
+        int i = 0, j = 0, count = 0;
         while (i < p.length() && j < t.length()) {
-            if (p.charAt(i) == t.charAt(j)) {
+            if (j == 0 || p.charAt(i) == t.charAt(j)) {
                 i++;
                 j++;
             } else {
-                i = i - j + 1;
-                j = next[i];
-                logger.info("next-->" + next[i]);
+                j = next[j];
             }
+            count++;
             if (j == t.length()) {
+                logger.info("count-->" + count);
                 return i - j;
             }
         }
@@ -33,7 +36,7 @@ public class KMPSample {
     }
 
     private static int violentMatch(@NotNull String p, String t) {
-        int i = 0, j = 0;
+        int i = 0, j = 0, count = 0;
         while (i < p.length() && j < t.length()) {
             if (p.charAt(i) == t.charAt(j)) {
                 i++;
@@ -42,24 +45,26 @@ public class KMPSample {
                 i = i - j + 1;
                 j = 0;
             }
-            if (j == t.length()) {
-                return i - j;
-            }
+            count++;
+        }
+        if (j == t.length()) {
+            logger.info("count-->" + count);
+            return i - j;
         }
         return -1;
     }
 
-    //@TODO: 2019-7-17 写不出来了，先放着
     private static int[] get_next(String p) {
         int[] next = new int[p.length()];
-        int i = 1, j = 0;
-        while (i < p.length()) {
-            if (j == 0 || p.charAt(i) == p.charAt(j)) {
+        next[0] = -1;
+        int i = 0, j = -1;
+        while (i < p.length() - 1) {
+            if (j == -1 || p.charAt(i) == p.charAt(j)) {
                 ++i;
                 ++j;
-                next[i - 1] = j;
+                next[i] = j;
             } else {
-                j = next[j - 1];
+                j = next[j];
             }
         }
         return next;
